72d6be47c9f44e329f4edc761c9167ddd50d469f
Nataliia Oliinyk <nataliia.oliinyk.de@gmail.com>
2025-02-14 17:10:53 +0100

    update backup_logs.sh - add task Очищаем файл который архивируем

--- Проект/backup_logs.sh
+++ Проект/backup_logs.sh
@@ -8,5 +8,8 @@ BACKUP_FILE="/var/log/httpd/backup-$date.tar.gz"
 # Архивируем лог файлы
 tar -czf "$TARGET_FILE" "$BACKUP_FILE"
 
+# Очищаем файл который архивируем
+> "$TARGET_FILE"
+
 # Удаляем старые архивы
 find "$BACKUP_FILE" -maxdepth 1 -type f -name "*.tar.gz" -mtime +3 -delete
